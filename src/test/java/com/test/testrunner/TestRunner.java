379e0c41def23da2859ed1aa0b372702ae79b889
shivanginigam4 <shivanginigam4@gmail.com>
2021-04-16 23:48:33 +0530

    Initial commit-update hubspot

--- src/test/java/com/test/testrunner/TestRunner.java
+++ src/test/java/com/test/testrunner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         monochrome = true,
-        plugin = {"html: target/cucumber.html"},
+        plugin = {"html: target/cucumber.html", "json: target/cucumber.json"},
         tags = {"@run1"},
         glue = {"com/test/stepDefs/"},
         features = "src/test/resources/Features")
